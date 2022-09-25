be076139f921bcfb53ea52de48697010cf2cd199
Henrique Pimentel <97296384+HenriqueNotFound@users.noreply.github.com>
2022-09-25 17:28:07 -0300

    adc. apagar

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function display(value) {
     document.getElementById("result").value += value;
 }
 
+// Função irá apagar o último dígito do display
+function apagarDigito() {
+    let currentNum = document.getElementById("result").value;
+    currentNum = currentNum.slice(0, -1);
+    document.getElementById("result").value = currentNum;
+}
+
 // Função que vai analisar a expressão do display e vai retornar o resultado
 function calcular() {
     let expressao = document.getElementById("result").value;
